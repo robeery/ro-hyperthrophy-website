d99f4a94f329e952cd07f9c30e1dcad8a4900ba6
Robert Constantin <robert-constantin.grigoras@student.tuiasi.ro>
2025-03-23 21:29:27 +0200

    lab 5 tema 1 + modificari css

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function ora()
     var s = d.getSeconds();
     var m = d.getMinutes();
     var h = d.getHours();
-    span.textContent = (" "+h).substring(-2) + ":" + (""+ m).substring(-2) + ":" + ("" + s).substring(-2);
+    span.innerHTML = (" "+h).substring(-2) + ":" + (""+ m).substring(-2) + ":" + ("" + s).substring(-2);
 }
 setInterval(ora, 10)
 var span2 = document.getElementById("data");
@@ -16,13 +16,62 @@ function data()
     var zi = d2.getDate();
     var luna = d2.getMonth()+1;
     var an = d2.getFullYear();
-    span2.textContent = (""+zi).substring() + "." + (""+luna).substring() + "." +(""+an).substring();
+    span2.innerHTML = (""+zi).substring() + "." + (""+luna).substring() + "." +(""+an).substring();
 
 }
 setInterval(data, 10);
+var url = document.getElementById("url");
+
 function getURL()
 {
-    let url = document.getElementById("url");
-    url.innerHTML="Pagina: " +  window.location;
-    url.textContent="Pagina: " + window.location;
+   url.innerHTML="URL: " +window.location.href; 
+
 }
+setInterval(getURL, 10);
+var browser = document.getElementById("browser");
+
+function getBrowser()
+{
+  browser.innerHTML="Browser: " +window.navigator.userAgent; 
+  //browser.innerHTML="Browser: ";
+
+}
+setInterval(getBrowser, 10);
+
+var location1 = document.getElementById("location");
+
+/*
+function getLocation()
+{
+  
+  //browser.innerHTML="Browser: ";
+  if (window.navigator.geolocation) {
+    
+    location1.innerHTML="Locație: " +window.navigator.geolocation; 
+
+  } else {
+     location1.innerHTML="Locația nu este disponibilă :(";
+  }
+
+
+}
+*/
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+            document.getElementById("location").innerHTML = 
+                `Locație: Latitudine ${lat}, Longitudine ${lon}`;
+        },
+        function (error) {
+            document.getElementById("location").innerHTML = 
+                "Eroare: Locația nu poate fi accesată.";
+        }
+    );
+} else {
+    document.getElementById("location").innerHTML = 
+        "Locația nu este disponibilă :(";
+}
+setInterval(getLocation, 10);
+
